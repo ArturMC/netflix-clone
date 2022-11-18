9b77518f7ad833388eeb1be71a20f1c02df6d64b
Artur Chojecki <amchojecki@gmail.com>
2022-11-17 21:42:17 -0500

    Initial app setup with React, Redux, and Firebase.

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzEY6gj98OiOOpARyrIPFdCfYegt8-t9w",
+  authDomain: "netflix-clone-8898d.firebaseapp.com",
+  projectId: "netflix-clone-8898d",
+  storageBucket: "netflix-clone-8898d.appspot.com",
+  messagingSenderId: "1043437614712",
+  appId: "1:1043437614712:web:02bc8e02a0314dac361f8d",
+};
