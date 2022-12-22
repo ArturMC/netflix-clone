d18494c77d7bb16ebd81b04798f9ae6077afda13
Artur Chojecki <amchojecki@gmail.com>
2022-12-22 14:43:58 -0500

    created signup screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import "./App.css";
-import HomeScreen from "./screens/HomeScreen";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
+import "./App.css";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+      } else {
+        // Logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDzEY6gj98OiOOpARyrIPFdCfYegt8-t9w",
   authDomain: "netflix-clone-8898d.firebaseapp.com",
@@ -6,3 +10,9 @@ const firebaseConfig = {
   messagingSenderId: "1043437614712",
   appId: "1:1043437614712:web:02bc8e02a0314dac361f8d",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
