f9f76a924a172a5391fc00e5e4546b53d92acbeb
Artur Chojecki <amchojecki@gmail.com>
2022-12-22 23:43:39 -0500

    Implemented login functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import "./App.css";
-import { auth } from "./firebase";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         // Logged in
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
         console.log(userAuth);
       } else {
         // Logged out
+        dispatch(logout);
       }
     });
 
